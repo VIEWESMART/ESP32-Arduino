3f06df482e9dc1bade0ab0bf06e293d5c959637e
VIEWE-CN <rd@chinasunyee.com>
2024-11-19 15:38:39 +0800

    add 7inch WIFI example

---
+++ examples/7.0inch/lvgl_wifi/lvgl_wifi.ino
@@ -0,0 +1,194 @@
+/**
+ * # LVGL Porting Example
+ *
+ * The example demonstrates how to port LVGL(v8). And for RGB LCD, it can enable the avoid tearing function.
+ *
+ * ## How to Use
+ *
+ * To use this example, please firstly install the following dependent libraries:
+ *
+ * - lvgl (>= v8.3.9, < v9)
+ *
+ * Then follow the steps below to configure:
+ *
+ * Follow the steps below to configure:
+ *
+ * 1. For **ESP32_Display_Panel**:
+ *
+ *     - Follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-drivers) to configure drivers if needed.
+ *     - If using a supported development board, follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#using-supported-development-boards) to configure it.
+ *     - If using a custom board, follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#using-custom-development-boards) to configure it.
+ *
+ * 2. For **lvgl**:
+ *
+ *     - Follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-lvgl) to add *lv_conf.h* file and change the configurations.
+ *     - Modify the macros in the [lvgl_port_v8.h](./lvgl_port_v8.h) file to configure the LVGL porting parameters.
+ *
+ * 3. Navigate to the `Tools` menu in the Arduino IDE to choose a ESP board and configure its parameters. For supported
+ *    boards, please refter to [Configuring Supported Development Boards](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-supported-development-boards)
+ * 4. Verify and upload the example to your ESP board.
+ *
+ * ## Serial Output
+ *
+ * ```bash
+ * ...
+ * LVGL porting example start
+ * Initialize panel device
+ * Initialize LVGL
+ * Create UI
+ * LVGL porting example end
+ * IDLE loop
+ * IDLE loop
+ * ...
+ * ```
+ *
+ * ## Troubleshooting
+ *
+ * Please check the [FAQ](https://github.com/esp-arduino-libs/ESP32_Display_Panel#faq) first to see if the same question exists. If not, please create a [Github issue](https://github.com/esp-arduino-libs/ESP32_Display_Panel/issues). We will get back to you as soon as possible.
+ *
+ */
+
+#include <Arduino.h>
+#include <ESP_Panel_Library.h>
+#include <lvgl.h>
+#include "lvgl_port_v8.h"
+
+/**
+/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ */
+#include <demos/lv_demos.h>
+// #include <examples/lv_examples.h>
+#include <WiFi.h>
+//无线网的名字和密码
+const char* ssid     = "V587";
+const char* password = "12345678";
+
+WiFiServer server(80);
+
+#define TFT_BL 2
+
+void wifi_ctr_loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+ pinMode(TFT_BL, OUTPUT);
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<font size=\"15\">Click <a href=\"/H\">here</a> to turn the screen ON.<br>");
+            client.print("<font size=\"15\">Click <a href=\"/L\">here</a> to turn the screen OFF.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(2, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(2, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+void setup()
+{
+    String title = "LVGL porting example";
+
+    Serial.begin(115200);
+    Serial.println(title + " start");
+
+    Serial.println("Initialize panel device");
+    ESP_Panel *panel = new ESP_Panel();
+    panel->init();
+#if LVGL_PORT_AVOID_TEAR
+    // When avoid tearing function is enabled, configure the RGB bus according to the LVGL configuration
+    ESP_PanelBus_RGB *rgb_bus = static_cast<ESP_PanelBus_RGB *>(panel->getLcd()->getBus());
+    rgb_bus->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
+    rgb_bus->configRgbBounceBufferSize(LVGL_PORT_RGB_BOUNCE_BUFFER_SIZE);
+#endif
+    panel->begin();
+
+    Serial.println("Initialize LVGL");
+    lvgl_port_init(panel->getLcd(), panel->getTouch());
+    pinMode(TFT_BL, OUTPUT);c:\Users\huo\Videos\2024-11-18 15-38-38.mkv
+    analogWrite(TFT_BL,255);
+    Serial.println("Create UI");
+    /* Lock the mutex due to the LVGL APIs are not thread-safe */
+    lvgl_port_lock(-1);
+
+    /* Create a simple label */
+    // lv_obj_t *label = lv_label_create(lv_scr_act());
+    // lv_label_set_text(label, title.c_str());
+    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    /**
+     * Try an example. Don't forget to uncomment header.
+     * See all the examples online: https://docs.lvgl.io/master/examples.html
+     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples
+     */
+    //  lv_example_btn_1();
+
+    /**
+     * Or try out a demo.
+     * Don't forget to uncomment header and enable the demos in `lv_conf.h`. E.g. `LV_USE_DEMO_WIDGETS`
+     */
+    lv_demo_widgets();
+    // lv_demo_benchmark();
+    // lv_demo_music();
+    // lv_demo_stress();
+
+    /* Release the mutex */
+    lvgl_port_unlock();
+
+    Serial.println(title + " end");
+    
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+}
+
+void loop()
+{
+    wifi_ctr_loop();
+
+    Serial.println("IDLE loop");
+    delay(1000);
+}
